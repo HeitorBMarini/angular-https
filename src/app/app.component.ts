d6c2d58ebfd6b092f0d379519a3b31e68871a706
HeitorBMarini <mariniheitor@gmail.com>
2025-05-12 15:11:48 -0300

    Iniciando projeto, add tailwind e come~ço de requisiçãohttps, api de paises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CountriesService } from './services/countries.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corrigido aqui
 })
-export class AppComponent {
-  title = 'angular-https';
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor(
+    private readonly _countrieService: CountriesService
+  ) {}
+
+  ngOnInit() {
+    this._countrieService.getCountries().subscribe((countriesResponse) => {
+      console.log('countriesResponse', countriesResponse);
+    });
+  }
 }
