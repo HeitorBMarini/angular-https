af461d730628a3c166bd1e699f310ded40227a9c
HeitorBMarini <mariniheitor@gmail.com>
2025-05-15 13:56:09 -0300

    Criação componente de informações gerais

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { PipesModule } from "../pipes/pipes.module";
 
 @NgModule({
   declarations: [],  
-  imports: [CommonModule],      
+  imports: [CommonModule, PipesModule],      
 })
 
 export class ComponentsModule {}
--- src/app/components/general-informations/general-informations.component.ts
+++ src/app/components/general-informations/general-informations.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserInfoItemComponent } from "../user-info-item/user-info-item.component";
+import { IUser } from '../../interfaces/user/user.interface';
+import { MaritalStatusPipe } from "../../pipes/marital-status.pipe";
 
 @Component({
   selector: 'app-general-informations',
-  imports: [],
+  imports: [UserInfoItemComponent, MaritalStatusPipe],
   templateUrl: './general-informations.component.html',
   styleUrl: './general-informations.component.css'
 })
 export class GeneralInformationsComponent {
-
+@Input({required: true}) user: IUser | undefined = {} as IUser;
 }
