e9afbf1dc1a6b043e9970dd5c0bb660c30ad8c3e
HeitorBMarini <mariniheitor@gmail.com>
2025-05-16 14:28:11 -0300

    Criação pipe de cpf

--- src/app/components/dependents-list/dependents-list.component.ts
+++ src/app/components/dependents-list/dependents-list.component.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { UserInfoItemComponent } from "../user-info-item/user-info-item.component";
 import { CommonModule } from '@angular/common';
 import { DependentsList } from '../../types/dependents-list';
+import { CpfPipe } from "../../pipes/cpf.pipe";
 
 @Component({
   selector: 'app-dependents-list',
-  imports: [UserInfoItemComponent, CommonModule],
+  imports: [UserInfoItemComponent, CommonModule, CpfPipe],
   templateUrl: './dependents-list.component.html',
   styleUrl: './dependents-list.component.css'
 })
