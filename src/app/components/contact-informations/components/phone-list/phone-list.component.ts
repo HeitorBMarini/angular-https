d18392d0523341103c901746b84752ef3f5082e2
HeitorBMarini <mariniheitor@gmail.com>
2025-05-16 11:29:59 -0300

    Lógica de Telefones

--- src/app/components/contact-informations/components/phone-list/phone-list.component.ts
+++ src/app/components/contact-informations/components/phone-list/phone-list.component.ts
@@ -1,15 +1,53 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PhoneList } from '../../../../types/phone-list';
-import { UserInfoItemComponent } from "../../../user-info-item/user-info-item.component";
+import { UserInfoItemComponent } from '../../../user-info-item/user-info-item.component';
 import { CommonModule } from '@angular/common';
+import { PhoneTypeEnum } from '../../../../enums/phone-type.enum';
+import { IPhone } from '../../../../interfaces/user/phone.interface';
+import { IPhoneToDisplay } from '../../../../interfaces/phone-to-display.interface';
+import { phoneTypeDescriptionMap } from '../../../../utils/phone-type-description-map';
+
 
 @Component({
   selector: 'app-phone-list',
   standalone: true,
   templateUrl: './phone-list.component.html',
   styleUrl: './phone-list.component.css',
-  imports: [UserInfoItemComponent, CommonModule]
+  imports: [UserInfoItemComponent, CommonModule],
 })
-export class PhoneListComponent {
-@Input({required: true}) userPhoneList: PhoneList = [];
+export class PhoneListComponent implements OnChanges {
+  phoneListToDisplay: IPhoneToDisplay[] = [];
+
+  @Input({ required: true }) userPhoneList: PhoneList | undefined = [];
+
+  ngOnChanges(changes: SimpleChanges) {
+    const PHONE_LIST_LOADED = Array.isArray(
+      changes['userPhoneList'].currentValue
+    );
+
+    if (PHONE_LIST_LOADED) {
+      this.preparePhoneListToDisplay();
+    }
+  }
+  preparePhoneListToDisplay() {
+    this.phoneListToDisplay = [];
+
+    
+    Object.keys(phoneTypeDescriptionMap)
+      .map(Number)
+      .forEach((phoneType: number) => {
+        const phoneFound = this.userPhoneList?.find(
+          (userPhone: IPhone) => userPhone.type === phoneType
+        );
+        this.phoneListToDisplay.push({
+          type: phoneTypeDescriptionMap[phoneType as PhoneTypeEnum],
+          phoneNumber: phoneFound ? this.formatPhoneNumber(phoneFound) : '-',
+        });
+      });
+
+  }
+
+  formatPhoneNumber(phone: IPhone) {
+    return `${phone.internationalCode} ${phone.areaCode} ${phone.number}`;
+  }
 }
