881407c89f0dd4315eb66a3ce943afd1f6322105
HeitorBMarini <mariniheitor@gmail.com>
2025-05-20 15:31:37 -0300

    Carregamento e autocomplete de paises

--- src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,28 +1,80 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CountriesList } from '../../types/countries-list';
 
 @Component({
   selector: 'app-general-informations-edit',
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatAutocompleteModule, MatRadioModule, MatDatepickerModule, ReactiveFormsModule, CommonModule],
-    providers: [provideNativeDateAdapter()],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './general-informations-edit.component.html',
-  styleUrl: './general-informations-edit.component.css'
+  styleUrl: './general-informations-edit.component.css',
 })
-export class GeneralInformationsEditComponent {
+export class GeneralInformationsEditComponent implements OnInit, OnChanges {
 
-  @Input({required: true }) userForm!: FormGroup;
+
+  countriesListFiltered: CountriesList = [];
+
+  @Input({ required: true }) userForm!: FormGroup;
+  @Input({ required: true }) countriesList: CountriesList = [];
+
+  ngOnInit(){
+        this.watchCountryFormChangesAndFilter();
+
+  }
+
+  ngOnChanges() {
+    console.log(this.userForm);
+
+    this.countriesListFiltered = this.countriesList;
+  }
 
   get emailControl(): FormControl {
     return this.userForm.get('generalInformations.email') as FormControl;
   }
+
+  get countryControl(): FormControl {
+    return this.userForm.get('generalInformations.country') as FormControl;
+  }
+
+  onCountrySelected(event: MatAutocompleteSelectedEvent) {
+console.log(event.option.value)
 }
 
+  private watchCountryFormChangesAndFilter() {
+    this.countryControl.valueChanges.subscribe(this.filterCountriesList.bind(this));
+  }
+
+  private filterCountriesList(searchTerm:string){
+ this.countriesListFiltered = this.countriesList.filter((country) =>
+      country.name.toLowerCase().includes(searchTerm.toLowerCase().trim())
+    );
+  }
+}
--- src/app/components/user-infomations-container/user-infomations-container.component.ts
+++ src/app/components/user-infomations-container/user-infomations-container.component.ts
@@ -1,37 +1,73 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { IUser } from '../../interfaces/user/user.interface';
 
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
-import { GeneralInformationsComponent } from "../general-informations/general-informations.component";
-import { ContactInformationsComponent } from "../contact-informations/contact-informations.component";
-import { DependentsListComponent } from "../dependents-list/dependents-list.component";
-import { GeneralInformationsEditComponent } from "../general-informations-edit/general-informations-edit.component";
-import { ContactInformationsEditComponent } from "../contact-informations-edit/contact-informations-edit.component";
-import { DependentsListEditComponent } from "../dependents-list-edit/dependents-list-edit.component";
+import { GeneralInformationsComponent } from '../general-informations/general-informations.component';
+import { ContactInformationsComponent } from '../contact-informations/contact-informations.component';
+import { DependentsListComponent } from '../dependents-list/dependents-list.component';
+import { GeneralInformationsEditComponent } from '../general-informations-edit/general-informations-edit.component';
+import { ContactInformationsEditComponent } from '../contact-informations-edit/contact-informations-edit.component';
+import { DependentsListEditComponent } from '../dependents-list-edit/dependents-list-edit.component';
 import { UserFormController } from './user.form-controller';
+import { CountriesService } from '../../services/countries.service';
+import { take } from 'rxjs';
+import { CountriesList } from '../../types/countries-list';
 
 @Component({
   selector: 'app-user-infomations-container',
-  imports: [CommonModule, MatTabsModule, GeneralInformationsComponent, ContactInformationsComponent,
-     DependentsListComponent, GeneralInformationsEditComponent, ContactInformationsEditComponent, DependentsListEditComponent],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    GeneralInformationsComponent,
+    ContactInformationsComponent,
+    DependentsListComponent,
+    GeneralInformationsEditComponent,
+    ContactInformationsEditComponent,
+    DependentsListEditComponent,
+  ],
   templateUrl: './user-infomations-container.component.html',
-  styleUrl: './user-infomations-container.component.css'
+  styleUrl: './user-infomations-container.component.css',
 })
-export class UserInfomationsContainerComponent extends UserFormController implements OnChanges{
-
+export class UserInfomationsContainerComponent
+  extends UserFormController
+  implements OnInit, OnChanges
+{
   currentTabIndex: number = 0;
 
-    @Input({required: true }) isInEditMode: boolean = false;
-  @Input({required: true }) userSelected: IUser = {} as IUser;
+  countriesList: CountriesList = [];
+  
+  private readonly _countriesService = inject(CountriesService)
 
-    ngOnChanges(changes: SimpleChanges) {
-      this.currentTabIndex = 0;
+  @Input({ required: true }) isInEditMode: boolean = false;
+  @Input({ required: true }) userSelected: IUser = {} as IUser;
 
-      const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
 
-      if(HAS_USER_SELECTED) {
-        this.fulfillUserForm(this.userSelected);
-      }
+  ngOnInit() {
+
+    this.getCountriesList();
+  }
+ 
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.currentTabIndex = 0;
+
+    const HAS_USER_SELECTED =
+      changes['userSelected'] &&
+      Object.keys(changes['userSelected'].currentValue).length > 0;
+
+    if (HAS_USER_SELECTED) {
+      this.fulfillUserForm(this.userSelected);
+    }
+  }
+
+ private  getCountriesList() {
+    this._countriesService.getCountries().pipe(take(1)).subscribe((countriesList: CountriesList) => {
+      this.countriesList = countriesList;
+    });
+
   }
 }
+
+
+
